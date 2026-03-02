1c7494220007ea4a1da99ff6f43a35b8c8dd93d3
Popov R&D <andrii@popov-rnd.com>
2026-03-02 11:56:41 +0200

    Fixed clients

--- src/main/java/com/popovrnd/quarkusaotbenchmark/web/controller/IOResource.java
+++ src/main/java/com/popovrnd/quarkusaotbenchmark/web/controller/IOResource.java
@@ -2,6 +2,7 @@ package com.popovrnd.quarkusaotbenchmark.web.controller;
 
 import io.smallrye.common.annotation.RunOnVirtualThread;
 import io.smallrye.mutiny.Uni;
+import jakarta.annotation.PostConstruct;
 import jakarta.ws.rs.GET;
 import jakarta.ws.rs.Path;
 import jakarta.ws.rs.core.Response;
@@ -20,47 +21,49 @@ public class IOResource {
     private static final Logger log =
             LoggerFactory.getLogger(IOResource.class);
 
-    private static final String WAITING_URI = "http://127.0.0.1:8081/waiting";
+    private HttpClient client;
 
-    // Single reusable client (transport-neutral baseline)
-    private static final HttpClient CLIENT =
-            HttpClient.newBuilder()
-                    .version(HttpClient.Version.HTTP_1_1)
-                    .build();
+    private HttpRequest request;
 
-    // Reusable immutable request
-    private static final HttpRequest REQUEST =
-            HttpRequest.newBuilder()
-                    .uri(URI.create(WAITING_URI))
-                    .GET()
-                    .build();
+    @PostConstruct
+    void init() {
+        client = HttpClient.newBuilder()
+                .version(HttpClient.Version.HTTP_1_1)
+                .build();
+        request =
+                HttpRequest.newBuilder()
+                        .uri(URI.create( "http://127.0.0.1:8081/waiting"))
+                        .GET()
+                        .build();
 
-    @RestClient
+    }
+
+    /*@RestClient
     WaitingClient waitingClient;
 
     @GET
     public Uni<Response> getNonBlocking() {
 
-        log.info("IO is called! Async signature. Thread = {}", Thread.currentThread());
+        //log.info("IO is called! Async signature. Thread = {}", Thread.currentThread());
 
         return waitingClient.waiting()
                 .map(r -> (r.getStatus() == 200)
                         ? Response.ok().build()
                         : Response.status(500).build());
-    }
+    }*/
 
 
     // Synchronous signature => runs on worker thread by default
-    /*@GET
+    @GET
     @RunOnVirtualThread
     public Response getBlocking() throws Exception {
 
         //log.info("IO is called! Synchronous signature. Thread = {}", Thread.currentThread());
 
-        int sc = CLIENT.send(REQUEST, HttpResponse.BodyHandlers.discarding()).statusCode();
+        int sc = client.send(request, HttpResponse.BodyHandlers.discarding()).statusCode();
 
         return sc == 200 ? Response.ok().build() : Response.status(500).build();
-    }*/
+    }
 
 }
 
